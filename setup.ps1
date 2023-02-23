d037b0bfc83efafc7bdcb25e3aa74819012959cd
danielgleason <dagleason9@gatech.edu>
2023-02-23 11:21:00 -0800

    Updated setup script to start the executable after building it

--- setup.ps1
+++ setup.ps1
@@ -3,3 +3,7 @@ python -m poetry install
 
 Write-Output "Building executable"
 python -m PyInstaller main.py --onefile --name "OMSCS-7210_Setup"
+
+Push-Location dist/
+./OMSCS-7210_Setup
+Pop-Location
